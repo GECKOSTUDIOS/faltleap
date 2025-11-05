2452152c5571e61279587295d75e1e8c20aa0312
Ramon Ott <ramon@julith.ch>
2025-11-05 09:08:35 +0100

    removoed migration folder

--- viewswewant/sensortypes/edit.leap.php
+++
@@ -1,8 +0,0 @@
-<form method="post" action="/sensortypes/edit/<?php echo $this->data->idsensortypes ?? ''; ?>">
-    <div class="form-group mb-3">
-        <label for="sensortype">Sensortyp</label>
-        <input type="text" class="form-control" id="sensortype" name="sensortype" value="<?php echo $this->data->sensortype ?? ''; ?>" required>
-    </div>
-    <button type="submit" class="btn btn-primary">Speichern</button>
-    <a href="/sensortypes" class="btn btn-secondary">Abbrechen</a>
-</form>
